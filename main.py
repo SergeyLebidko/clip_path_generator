b12e0fca79b5b82721a0db1ba7d211beff7f1167
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-06 19:15:42 +0300

    Создан каркас необходимых функций

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print('Привет!')
+ROW_COUNT = 25
+COL_COUNT = 25
+
+CELL_SIZE = 4
+
+
+def field_gen():
+    field = {}
+    for r in range(ROW_COUNT):
+        for c in range(COL_COUNT):
+            key = (r, c)
+            field[key] = True
+
+    # TODO Код генерирования очередного состояния поля
+
+
+def get_polygon_data(field):
+    # TODO Код преобразования переданного состояния поля в список координат ломанной
+    return []
+
+
+def main():
+    result = []
+    for field in field_gen():
+        polygon_data = get_polygon_data(field)
+        result.append(polygon_data)
+
+    # TODO Код записи на диск выходного JSON-файла
+
+
+if __name__ == '__main__':
+    main()
