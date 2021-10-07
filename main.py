aa43c530fb5fb9f0a010c6b0a353dca8ba7b770a
SergeyLebidko <it.sergeyler@mail.ru>
2021-10-07 14:51:22 +0300

    Добавлен код формирования списка отрезков

--- main.py
+++ main.py
@@ -6,23 +6,64 @@ CELL_SIZE = 4
 
 def field_gen():
     field = {}
-    for r in range(ROW_COUNT):
-        for c in range(COL_COUNT):
-            key = (r, c)
+    for row in range(ROW_COUNT):
+        for col in range(COL_COUNT):
+            key = (row, col)
             field[key] = True
 
     yield field
 
-    for s in range(0, COL_COUNT + ROW_COUNT - 2):
-        for r in range(ROW_COUNT):
-            for c in range(COL_COUNT):
-                field[(r, c)] = (r + c) > s
+    for sum_limit in range(0, COL_COUNT + ROW_COUNT - 2):
+        for row in range(ROW_COUNT):
+            for col in range(COL_COUNT):
+                field[(row, col)] = (row + col) > sum_limit
 
         yield field
 
 
 def get_polygon_data(field):
-    # TODO Код преобразования переданного состояния поля в список координат ломанной
+    # Формирование списка отрезков
+    segments_set = set()
+    for row in range(ROW_COUNT):
+        for col in range(COL_COUNT):
+            if not field[(row, col)]:
+                continue
+
+            # Верхняя граница ячейки
+            if not field.get((row - 1, col), False):
+                x1 = col * CELL_SIZE
+                y1 = row * CELL_SIZE
+                x2 = (col + 1) * CELL_SIZE
+                y2 = row * CELL_SIZE
+                segments_set.add(((x1, y1), (x2, y2)))
+
+            # Правая граница ячейки
+            if not field.get((row, col + 1), False):
+                x1 = (col + 1) * CELL_SIZE
+                y1 = row * CELL_SIZE
+                x2 = (col + 1) * CELL_SIZE
+                y2 = (row + 1) * CELL_SIZE
+                segments_set.add(((x1, y1), (x2, y2)))
+
+            # Нижняя граница ячейки
+            if not field.get((row + 1, col), False):
+                x1 = col * CELL_SIZE
+                y1 = (row + 1) * CELL_SIZE
+                x2 = (col + 1) * CELL_SIZE
+                y2 = (row + 1) * CELL_SIZE
+                segments_set.add(((x1, y1), (x2, y2)))
+
+            # Левая граница ячейки
+            if not field.get((row, col - 1), False):
+                x1 = col * CELL_SIZE
+                y1 = row * CELL_SIZE
+                x2 = col * CELL_SIZE
+                y2 = (row + 1) * CELL_SIZE
+                segments_set.add(((x1, y1), (x2, y2)))
+
+    # TODO Тестовый код. В дальнейшем - удалить
+    # print('Количество сегментов:', len(segments_set), '\n')
+
     return []
 
 
@@ -30,12 +71,11 @@ def main():
     result = []
     for field in field_gen():
 
-        for r in range(ROW_COUNT):
-            for c in range(COL_COUNT):
-                print('X' if field[(r, c)] else '.', end='')
-            print()
-
-        print()
+        # TODO Тестовый код. В дальнейшем - удалить
+        # for r in range(ROW_COUNT):
+        #     for c in range(COL_COUNT):
+        #         print('X' if field[(r, c)] else '.', end='')
+        #     print()
 
         polygon_data = get_polygon_data(field)
         result.append(polygon_data)
